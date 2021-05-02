08d9eabfc0151281a75c1dd1ea3e46f894bc9d82
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-05-03 00:17:56 +0530

    added h1 (test)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -17,6 +17,7 @@ function ExpenseItem(props) {
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{title}</h2>
+                <h1>sgbsbs</h1>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
